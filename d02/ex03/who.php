591a5895830069c7f7631c3f5f5727e4f78ec4ea
Salim Salaues <sesl@Skynet-T1000.local>
2017-06-08 23:09:04 -0700

    utmpx

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -2,10 +2,22 @@
 <?php
 $stuff = file_get_contents("/var/run/utmpx");
 date_default_timezone_set("America/Los Angeles");
-	while($stuff)
+while($stuff)
+{
+
+	$y = [];
+	date_default_timezone_set("Europe/Paris");
+	$x = unpack("A256user/A4id/A32ttyname/ipid/itype/lloginsec/lloginus/A256host/A64pad", $contents);
+	if ($x['type'] == 7)
+		$y[] = $x['user'] . " " . $x['ttyname'] . "  " . strftime("%b %e %R", $x['loginsec']) . PHP_EOL;
+	$contents = substr($contents, 628);
+	sort($y);
+	$i = 0;
+	while ($i < count($y))
 	{
-		
-		$y = [];
+		print($y[$i]);
+		$i++;
 	}
+}
 
 ?>
