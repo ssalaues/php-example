80989bb488ad24c303449f432f335a40f301b40c
Salim Salaues <ssalaues@users.noreply.github.com>
2017-06-10 19:18:34 -0700

    Value changes

--- d04/ex00/index.php
+++ d04/ex00/index.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	if ($_GET['submit'] === "OK")
+	if ($_GET['submit'] === "SENT")
 	{
 		$_SESSION["login"] = $_GET["login"];
 		$_SESSION{"passwd"] = $_GET["passwd"];
@@ -11,7 +11,7 @@
 		<form method="get">
 		Username: <input type="text" name="name" value="<?php echo $_SESSION['login'];?>"><br><br>
 		Password: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd'];?>">
-		<input type="submit" name="submit" value="Submit">
+		<input type="submit" name="submit" value="SENT">
 	</form>
 <?php
 echo "<h2>Your Input:</h2>";
