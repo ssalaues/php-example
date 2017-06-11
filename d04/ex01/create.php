3913d5ad5345242db5a41be8adaac648c311aff1
Salim Salaues <ssalaues@users.noreply.github.com>
2017-06-10 19:37:18 -0700

    Serialize this

--- d04/ex01/create.php
+++ d04/ex01/create.php
@@ -1,2 +1,25 @@
 <?php
+  function err()
+  {
+        echo "ERROR\n"
+        exit;
+  }
+?>
+<?php
+  if (!isset($_POST['submit']) || $_POST['submit'] !== "OK")
+  {
+        error();
+  }
+  if (!isset($_POST['login'] || !isset($_POST['passwd']))
+  {
+        error();
+  }
+  $serialpath = "../private/passwd";
+  if (file_exists($serialpath))
+      {
+        if (($file = file_get_contents($serialpath) !== FALSE)
+            {
+              $auth = unserialize($file);
+            }
+      }
 ?>
